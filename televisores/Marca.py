a8dcbb0f0fa53ca489b90a8e7af3bb54d730ea09
StivenSaldarriagaMayorga <ssaldarriagam@unal.edu.co>
2024-02-25 13:49:27 -0500

    prueba3

--- televisores/Marca.py
+++ televisores/Marca.py
@@ -1,5 +1,5 @@
 
-class marca:
+class Marca:
     def __init__(self, nombre:str):
         self.__nombre = nombre
 
--- televisores/TV.py
+++ televisores/TV.py
@@ -1,4 +1,4 @@
-class tv:
+class TV:
     numTV = 0
     def __init__(self, marca, estado:bool):
         self.__marca = marca
@@ -7,7 +7,7 @@ class tv:
         self.__estado = estado
         self.__volumen = 1
         self.__control = None   ##Con NONE se puede asignar despues el valor
-        tv.numTV += 1
+        TV.numTV += 1
 #### Marca
     def getterMarca(self):
         return self.__marca
