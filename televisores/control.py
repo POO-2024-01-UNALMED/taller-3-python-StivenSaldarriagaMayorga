5444fadebe67b793e010f8c833b2767f2ece95de
StivenSaldarriagaMayorga <ssaldarriagam@unal.edu.co>
2024-02-27 10:39:38 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,46 +1,26 @@
-
 class Control:
     def __init__(self):
-        self.__tv=None
-
-
+        self._tv = None
     def enlazar(self, tv):
-        self.__tv = tv
-        self.__tv.setControl(self)
-
+        self._tv = tv
+        self._tv.setControl(self)
     def getTv(self):
-        return self.__tv
-
-    def setTv(self, new_tv):
-        self.__tv = new_tv
-
-#############################################################
-    def canalDown(self):
-            self.__tv.canalDown()
-    
-    def canalUp(self):
-            self.__tv.canalUp()
-
-    def volumenUp(self):
-            self.__tv.volumenUp()
-    
-    def volumenDown(self):
-            self.__tv.volumenDown()
-   
-    def getCanal(self):
-        self.__tv.getCanal()
-
-    def setCanal(self, new_canal):
-            self.__tv.setCanal(new_canal)
-
-    def getVolumen(self):
-        self.__tv.getVolumen()
-
-    def setVolumen(self, new_volumen):
-        self.__tv.setVolumen(new_volumen)
-
-    def turnOff(self):
-            self.__tv.turnOff()
-
+        return self._tv
+    def setTv(self, tv):
+        self._tv = tv
     def turnOn(self):
-            self.__tv.turnOff()
+        self._tv.turnOn()
+    def turnOff(self):
+        self._tv.turnOff()
+    def canalUp(self):
+        self._tv.canalUp()
+    def canalDown(self):
+        self._tv.canalDown()
+    def volumenUp(self):
+        self._tv.volumenUp()
+    def volumenDown(self):
+        self._tv.volumenDown()
+    def setCanal(self, canal):
+        self._tv.setCanal(canal)
+    def setVolumen(self, volumen):
+        self._tv.setVolumen(volumen)
